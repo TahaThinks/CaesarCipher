0c73610942e4d5102ffcd8e2975f5d7bb9b7c1a2
TahaThinks <tahaa4587@gmail.com>
2024-01-09 10:17:37 +0400

    Add Decrypt function

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(plain_text, shift_amount):
     encrypted_message = ''
     for letter in plain_text:
@@ -12,4 +10,15 @@ def encrypt(plain_text, shift_amount):
         encrypted_message += new_value
     print(f"The encoded message is {encrypted_message}")
 
-encrypt(text,shift)
+def decrypt(plain_text, shift_amount):
+    encrypted_message = ''
+    for letter in plain_text:
+        new_value = chr(ord(letter) - shift_amount)
+        encrypted_message += new_value
+    print(f"The encoded message is {encrypted_message}")
+
+
+if direction == "encrypt":
+    encrypt(text, shift)
+else:
+    decrypt(text, shift) 
