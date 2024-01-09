fdb4596fa350f462a0e26cf7d9e7cff332534e23
TahaThinks <tahaa4587@gmail.com>
2024-01-09 09:46:14 +0400

    Starting File

--- test.py
+++
@@ -1,9 +0,0 @@
-# Review: 
-# Create a function called greet(). 
-def great():
-    print("Hello")
-    print("How is the weather")
-    print("Hope you have a great day")
-# Write 3 print statements inside the function.
-# Call the greet() function and run your code.
-great()
