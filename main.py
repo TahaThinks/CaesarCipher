e77875ccc95ca630674646e85b219a1ee1c7e878
TahaThinks <tahaa4587@gmail.com>
2024-01-09 10:37:38 +0400

    Simplify Code

--- main.py
+++ main.py
@@ -3,22 +3,33 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-    encrypted_message = ''
-    for letter in plain_text:
-        new_value = chr(ord(letter) + shift_amount)
-        encrypted_message += new_value
-    print(f"The encoded message is {encrypted_message}")
+def caeser (start_text, shift_amout, cipher_direction):
+    end_text = ''
+    if cipher_direction == 'decode': shift_amout *= -1
+    for letter in start_text:
+        new_value = chr(ord(letter) + shift_amout)
+        end_text += new_value
 
-def decrypt(plain_text, shift_amount):
-    encrypted_message = ''
-    for letter in plain_text:
-        new_value = chr(ord(letter) - shift_amount)
-        encrypted_message += new_value
-    print(f"The encoded message is {encrypted_message}")
+    print(f"The {cipher_direction}d message is {end_text}")
+    
+caeser(text, shift, direction)
+
+# def encrypt(plain_text, shift_amount):
+#     encrypted_message = ''
+#     for letter in plain_text:
+#         new_value = chr(ord(letter) + shift_amount)
+#         encrypted_message += new_value
+#     print(f"The encoded message is {encrypted_message}")
+
+# def decrypt(plain_text, shift_amount):
+#     encrypted_message = ''
+#     for letter in plain_text:
+#         new_value = chr(ord(letter) - shift_amount)
+#         encrypted_message += new_value
+#     print(f"The encoded message is {encrypted_message}")
 
 
-if direction == "encrypt":
-    encrypt(text, shift)
-else:
-    decrypt(text, shift) 
+# if direction == "encrypt":
+#     encrypt(text, shift)
+# else:
+#     decrypt(text, shift) 
