abbdc33c49edddbfe11c2a082339f5f2bb5cfeb7
TahaThinks <tahaa4587@gmail.com>
2024-01-09 08:41:09 +0400

    Create Function

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 # Review: 
 # Create a function called greet(). 
+def great():
+    print("Hello")
+    print("How is the weather")
+    print("Hope you have a great day")
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
+great()
